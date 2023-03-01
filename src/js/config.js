c8da7769611167e8eb186532f7242659ce691308
Md. ARif Hossain <mdarifhossain512bd@gmail.com>
2023-03-01 21:43:30 +0600

    initial updates

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '8a99920b-6636-4464-abd2-77f160b7efde';
 export const MODAL_CLOSE_SEC = 2.5;
